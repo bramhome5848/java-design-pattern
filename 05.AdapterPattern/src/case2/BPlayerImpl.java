c11aa5d5ddb0058e1ba2a36d760bebd41612232e
이경진 <bramhome5848@gmail.com>
2020-07-22 18:44:59 +0900

    20200722 05.AdapterPattern

---
+++ 05.AdapterPattern/src/case2/BPlayerImpl.java
@@ -0,0 +1,14 @@
+package case2;
+
+public class BPlayerImpl extends BPlayer {
+
+    @Override
+    public void playFile(String fileName) {
+        System.out.println("(B) " + fileName);
+    }
+
+    @Override
+    public void stopFile() {
+
+    }
+}
